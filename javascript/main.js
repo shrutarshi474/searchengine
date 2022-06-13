c62a4a719be2018ca5a6a6783a77fe33705b420a
Shrutarshi Ray <84886266+shrutarshi474@users.noreply.github.com>
2022-06-13 21:53:24 +0530

    add me

--- javascript/main.js
+++ javascript/main.js
@@ -1,65 +1,73 @@
-
-var input = document.querySelector(".js-go").addEventListener("click",function(){
-  var input = document.querySelector("input").value;
-  console.log(input);
-  document.querySelector("h1").innerHTML = input;
-  var url = "http://api.giphy.com/v1/gifs/search?q=\"" + input + "\"&api_key=QCVPMYygb8XsfDFUa2nheVFyzOovRMAi";
-  console.log(url);
-  var GiphyAJAXCall = new XMLHttpRequest();
-  GiphyAJAXCall.open( 'GET', url );
-  GiphyAJAXCall.send();
-
-  GiphyAJAXCall.addEventListener("load",function(e){
-    var data = e.target.response;
-    console.log(data);
-    printin(data);
-
-  });
-
-
-
-});
- document.querySelector(".js-userinput").addEventListener("keyup",function(event){
-
-  var input = document.querySelector("input").value;
-  console.log(input);
-  if(event.key === "Enter"){
-    document.querySelector("h1").innerHTML = input;
-
-
-    var url = "http://api.giphy.com/v1/gifs/search?q=\"" + input + "\"&api_key=QCVPMYygb8XsfDFUa2nheVFyzOovRMAi";
-    console.log(url);
-    var GiphyAJAXCall = new XMLHttpRequest();
-    GiphyAJAXCall.open( 'GET', url );
-    GiphyAJAXCall.send();
-
-    GiphyAJAXCall.addEventListener("load",function(e){
-      var data = e.target.response;
-      console.log(data);
-      printin(data);
-
-    })
-
-  }
-
-
+document.querySelector(".js-go").addEventListener('click', function() {
+	var inputValue = document.querySelector('.js-userinput').value;
+		var userInput = getUserInput();
+	searchGiphy( userInput );
 
 });
 
+document.querySelector('.js-userinput').addEventListener('keyup', function (e) {
+	if (e.which === 13) {
+ 		var userInput = getUserInput();
+ 		searchGiphy( userInput );
+    }
+});
+
+function getUserInput() {
+	var inputValue = document.querySelector('.js-userinput').value;
+
+	return inputValue;
+}
 
 
-function printin(print){
-  var respons = JSON.parse(print);
-  var imgeurl = respons.data;
-  var container = document.querySelector(".js-container");
-  imgeurl.forEach(function(image){
-    var src = image.images.fixed_height.url;
-
-    container.innerHTML += "<img src=\"" + src + "\"class=\"container-image\">";
 
 
 
-  })
+function searchGiphy( searchQuery ) {
+	var url = "http://api.giphy.com/v1/gifs/search?q=\"" + searchQuery + "\"&api_key=QCVPMYygb8XsfDFUa2nheVFyzOovRMAi";
+
+	// AJAX Request
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open( 'GET', url );
+	GiphyAJAXCall.send();
 
 
+	GiphyAJAXCall.addEventListener('load', function( data ) {
+
+			var actualData = data.target.response;
+			pushToDOM(actualData);
+			console.log(actualData);
+
+	});
+
+}
+
+
+
+
+
+
+
+
+
+
+function pushToDOM( response ) {
+	// turn response into real javascript object
+	response = JSON.parse( response );
+	// drill down to the data array
+	var images = response.data;
+
+	// find the container to hold this stuff in DOM
+	var container = document.querySelector('.js-container');
+	// clear it of old content since this function will be used on every search
+	// we want to reset the div
+	container.innerHTML = "";
+
+	// loop through data array and add IMG html
+	images.forEach(function(image){
+		// find img src
+		var src = image.images.fixed_height.url;
+
+		// concatenate a new IMG tag
+		container.innerHTML += "<img src='"+ src +"' class='container-image' />";
+	});
 }
